bdf4b85a670d10da4fc6549db76b199fb1d335f0
Alio Webdeveloper <aliowebdeveloper@gmail.com>
2016-11-06 14:44:35 +0600

    Sterted jQuejQuery adding snd edited styles

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const {app, BrowserWindow} = electron;
 const mongoose = require('./libs/mongoose');
+const argv = require('minimist')(process.argv.slice(2));
 
 var win = null;
 
@@ -13,20 +14,26 @@ app.on('window-all-closed', () => {
 app.on('ready', () => {
 	const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize;
 	let winOpts = {
-		width: width * .5,
 		height: height * .5,
-		//acceptFirstMouse: true,
 		titleBarStyle: 'hidden',
 		frame: false,
 		//'resizable': false,
 		//'kiosk': true,
 	};
 	if (process.platform === 'linux') {
-	  winOpts.icon = (`${__dirname}/assets/app-icon/png/128.png`);
+		winOpts.icon = (`${__dirname}/assets/app-icon/png/128.png`);
 	}
+	if (argv.dev) {
+		winOpts.width = 1300;
+	} else {
+		winOpts.width = width * .5;
+	}
+
 	win = new BrowserWindow(winOpts);
 	win.loadURL(`file://${__dirname}/index.html`);
-	win.setMenu(null);
+	if (argv.dev) {
+		win.webContents.openDevTools();
+	}
 	win.on('closed', () => {
 		win = null;
 	});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
+//const $ = jQuery = require('jquery');
 const {remote} = require('electron');
 const {BrowserWindow} = remote;
 const win = BrowserWindow.getFocusedWindow();
+//require('jquery-ui');
+/*require([ "jquery", "jquery-ui/datepicker" ], function( $ ) {
+    $( "<input>" )
+        .appendTo( "body" )
+        .datepicker();
+});*/
 
 var actions = document.getElementById('js-actions');
 
@@ -16,3 +23,18 @@ actions.addEventListener('click', function (e) {
 		win.maximize();
 	}
 });
+
+/*var select = $( "#edit-switcher" );
+
+var slider = $( "<div id='switcher'></div>" ).insertAfter( select ).slider({
+	min: 1,
+	max: 2,
+	range: "min",
+	value: select[ 0 ].selectedIndex + 1,
+	slide: function( event, ui ) {
+		select[ 0 ].selectedIndex = ui.value - 1;
+	}
+});
+$( "#edit-switcher" ).change(function() {
+	slider.slider( "value", this.selectedIndex + 1 );
+});*/
